a2bb9f56b048618ff9b3e21ce0bec6b2b262f803
amir87sls <joker87sls@gmail.com>
2026-08-20 20:16:48 +0330

    Delete diplomacy.py

--- diplomacy.py
+++
@@ -1,4 +0,0 @@
-from database import db,now,enabled
-def complaint(a,d,org,subject):
-    if not enabled('complaints'): return False
-    c=db(); c.execute('INSERT INTO complaints(from_country,to_country,organization,subject,created_at) VALUES(?,?,?,?,?)',(a,d,org,subject,now())); c.commit(); c.close(); return True
