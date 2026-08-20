20baa0a6ca32ed594a634637c72bf3db7243a915
amir87sls <joker87sls@gmail.com>
2026-08-20 20:16:29 +0330

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-BOT_TOKEN=os.getenv('BOT_TOKEN','').strip()
-OWNER_ID=int(os.getenv('OWNER_ID','0') or 0)
-NEWS_CHAT_ID=os.getenv('NEWS_CHAT_ID','').strip()
-DB_PATH=os.getenv('DB_PATH','nexora.db')
-PORT=int(os.getenv('PORT','10000'))
