318e4a7f0d84568e3f9d8f2e9002ad0854286a5c
amir87sls <joker87sls@gmail.com>
2026-08-20 20:16:16 +0330

    Delete alliances.py

--- alliances.py
+++
@@ -1,5 +0,0 @@
-from database import db
-def create(name,owner,description=''):
-    c=db(); cur=c.execute('INSERT INTO alliances(name,description,owner_id) VALUES(?,?,?)',(name,description,owner)); c.commit(); x=cur.lastrowid; c.close(); return x
-def add_member(aid,cid):
-    c=db(); c.execute('INSERT OR IGNORE INTO alliance_members VALUES(?,?)',(aid,cid)); c.commit(); c.close()
